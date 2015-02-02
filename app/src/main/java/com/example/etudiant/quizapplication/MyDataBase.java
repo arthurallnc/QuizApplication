2c53996c418a32ea926c8d7c5e58911cc61f6b4e
arthurallnc <arthur.allanic@gmail.com>
2015-02-02 16:23:37 +0100

    First commit

--- app/src/main/java/com/example/etudiant/quizapplication/MyDataBase.java
+++ app/src/main/java/com/example/etudiant/quizapplication/MyDataBase.java
@@ -1,7 +1,38 @@
 package com.example.etudiant.quizapplication;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by etudiant on 02/02/2015.
  */
-public class MyDataBase {
+public class MyDataBase extends SQLiteOpenHelper{
+
+    public static final String TABLE_QUESTIONS = "questions";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_INTITULE = "intitule";
+    public static final String COLUMN_ANSWER_A = "answer_a";
+    public static final String COLUMN_ANSWER_B = "answer_b";
+    public static final String COLUMN_ANSWER_C = "answer_c";
+    public static final String COLUMN_ANSWER_D = "answer_d";
+    public static final String COLUMN_TRUTH = "truth";
+
+    private static final String DATABASE_NAME = "commments.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_COMMENTS + "(" + COLUMN_ID
+            + " integer primary key autoincrement, " + COLUMN_COMMENT
+            + " text not null);";
+
+    public MyDataBase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
 }
